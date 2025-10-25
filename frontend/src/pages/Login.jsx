1197a115ba1b4ab4d831dbce72d78cc3dd66785f
Anwar Kamal <101911953+Anwar7550@users.noreply.github.com>
2025-10-25 23:19:33 +0500

    Change login URL to production endpoint

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
       return handleError("email or password are required!");
     }
     try {
-      const url = "http://localhost:5002/auth/login";
+      const url = "https://mern-auth-project-f4as.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
