c977d97e2f876688fb1c6b2d68fd3db21f74e419
744anwar <anwarkamal744@gmail.com>
2025-10-31 16:48:20 +0500

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mern-auth-project-f4as.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(bodyParser.json());
 
